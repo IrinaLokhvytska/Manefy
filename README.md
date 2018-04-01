ca507b620d2d21891fa1d179dc6f18a1c822705b
IrinaLokhvytska <irinaramzaeva2@gmail.com>
2018-04-01 20:28:36 +0300

    add docker

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Manefy
+#Docker
+1. Install Docker Toolbox for Windows
+2. Run Docker QuickStart
+3. Change directory and run `docker build -t monefy .`
+4. Run `docker run -p 4000:80 monefy`
+
+# Telegram
 Telegram Bot:
 name - MonefyBot
 username - monefy_bot
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from sanic import Sanic
+from sanic.response import json
 # import sqlalchemy
 # import alembic
 # import telepot
@@ -7,6 +8,10 @@ from sanic import Sanic
 # import os
 
 app = Sanic()
+
+@app.route('/')
+async def test(request):
+    return json({'hello': 'world'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)

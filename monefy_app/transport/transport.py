4169d95a74ffca20ca85930b04ba4e70dd7afd51
IrinaLokhvytska <irinaramzaeva2@gmail.com>
2018-04-05 22:45:20 +0300

    test dropbox

--- monefy_app/transport/transport.py
+++ monefy_app/transport/transport.py
@@ -3,9 +3,11 @@ from monefy_app import keys
 
 
 class DropboxTransport:
-    def upload_file_to_dropbox(self):
-        sess = dropbox.session.DropboxSession(keys.APP_KEY, keys.APP_SECRET, keys.ACCESS_TYPE)
-        sess.set_token(keys.access_token)
-        client = dropbox.client.DropboxClient(sess)
-        f = open('dropbox-upload.py')
-        client.put_file('/dropbox-upload.py', f)
+    def test_dropbox(self):
+        dbx = dropbox.Dropbox(keys.access_token)
+        return dbx.users_get_current_account()
+
+
+if __name__ == '__main__':
+    dropbox_class = DropboxTransport()
+    print(dropbox_class.test_dropbox())

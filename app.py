5fe6d5e9c4317bf7960769dd991a0ba8592dd2dd
Iryna Lokhvyts'ka <ilokh@softserveinc.com>
2018-04-10 15:05:29 +0300

    add flake8

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 from sanic import Sanic
 from sanic.response import json
-# import sqlalchemy
-# import alembic
-# import telepot
-# import flake8
-# import oauth2client
-# import os
 
 app = Sanic()
+
 
 @app.route('/')
 async def test(request):
